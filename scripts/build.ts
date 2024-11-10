da62c9678ad60cbbdbd816b99ab2126c14a46c5d
Reve <aquaplmc@gmail.com>
2024-11-10 23:43:36 +0700

    Update build.ts

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,17 +1,21 @@
 /// <reference types='bun-types' />
 import { existsSync, rmSync } from 'node:fs';
-import { exec } from './utils';
-import tsconfig from '../tsconfig.json';
+import { generate } from '@stacksjs/dtsx/generate.js';
 
 // Constants
 const SOURCEDIR = './src';
-const OUTDIR = tsconfig.compilerOptions.declarationDir;
+const OUTDIR = 'lib';
 
 // Remove old content
 if (existsSync(OUTDIR)) rmSync(OUTDIR, { recursive: true });
 
-// Emit declaration files
-exec`bun x tsc`;
+generate({
+  root: SOURCEDIR, // default: './src'
+  entrypoints: ['**/*'],
+  outdir: OUTDIR,
+  clean: true,
+  verbose: true
+});
 
 // Transpile files concurrently
 const transpiler = new Bun.Transpiler({
@@ -23,14 +27,14 @@ const transpiler = new Bun.Transpiler({
   treeShaking: true
 });
 
-for (const path of new Bun.Glob('**/*.ts').scanSync(SOURCEDIR))
+for (const path of new Bun.Glob('**/*.ts').scanSync(SOURCEDIR)) {
   Bun.file(`${SOURCEDIR}/${path}`)
     .arrayBuffer()
-    .then((buf) => transpiler.transform(buf)
+    .then(async (buf) => transpiler.transform(buf)
       .then((res) => {
         if (res.length !== 0) {
           const pathExtStart = path.lastIndexOf('.');
-          Bun.write(`${OUTDIR}/${path.substring(0, pathExtStart === -1 ? path.length : pathExtStart) + '.js'}`, res);
+          Bun.write(`${OUTDIR}/${`${path.substring(0, pathExtStart === -1 ? path.length : pathExtStart)}.js`}`, res);
         }
-      })
-    );
+      }));
+}
