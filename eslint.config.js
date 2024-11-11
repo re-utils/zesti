c0e0425fa49c85210ae95b93dc9ab7d6495f7e18
Reve <aquaplmc@gmail.com>
2024-11-11 14:19:18 +0700

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -165,7 +165,6 @@ export default tsEslint.config(
       "@typescript-eslint/no-namespace": "error",
       "@typescript-eslint/no-non-null-asserted-nullish-coalescing": "error",
       "@typescript-eslint/no-non-null-asserted-optional-chain": "error",
-      "@typescript-eslint/no-non-null-assertion": "error",
       "@typescript-eslint/no-redundant-type-constituents": "error",
       "@typescript-eslint/no-require-imports": "error",
       "@typescript-eslint/no-restricted-imports": "error",
