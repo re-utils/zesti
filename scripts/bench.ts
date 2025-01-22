c595be1f54ebd6e2c0298b12c7421354790b345e
aquapi <aquaplmc@gmail.com>
2025-01-22 22:48:57 +0700

    Test

--- scripts/bench.ts
+++ scripts/bench.ts
@@ -1,22 +1,50 @@
 import { Glob } from 'bun';
+import fs from 'fs';
 import { exec } from './utils';
 
-const DIR = import.meta.dir + '/../bench';
+const DIR = import.meta.dir + '/../bench/';
+const OUTDIR = DIR + '.out/';
+
+const args = process.argv;
 const exe = { raw: 'bun run' };
 
-let exactBench = process.argv[2];
-if (exactBench == '--node') {
-  exe.raw = 'bun tsx --expose-gc --allow-natives-syntax';
-  exactBench = process.argv[3];
+let doOutput = false;
+
+const run = async (path: string) => {
+  console.log('Running benchmark:', path);
+
+  if (doOutput) {
+    const target = OUTDIR + path.slice(0, path.indexOf('.bench.ts')) + '.out';
+    await Bun.write(target, '\0');
+    await Bun.write(target, '');
+    await exec`${exe} ${path} > ${Bun.file(target)}`
+  } else
+    await exec`${exe} ${path}`;
+}
+
+{
+  let idx = args.indexOf('--node');
+  if (idx !== -1) {
+    exe.raw = 'bun tsx --expose-gc --allow-natives-syntax';
+    args.splice(idx, 1);
+  }
+
+  idx = args.indexOf('--emit');
+  if (idx !== -1) {
+    // Load output directory
+    if (fs.existsSync(OUTDIR))
+      fs.rmdirSync(OUTDIR, { recursive: true });
+    fs.mkdirSync(OUTDIR);
+
+    doOutput = true;
+    args.splice(idx, 1);
+  }
 }
 
 Bun.$.cwd(DIR);
+const exactBench = args[2];
 
-if (exactBench != null) {
-  const path = `${exactBench}.bench.ts`;
-  console.log('Running benchmark:', path);
-  await exec`${exe} ${path}`;
-} else for (const path of new Glob('**/*.bench.ts').scanSync(DIR)) {
-  console.log('Running benchmark:', path);
-  await exec`${exe} ${path}`;
-}
+if (exactBench != null)
+  await run(`${exactBench}.bench.ts`);
+else for (const path of new Glob('**/*.bench.ts').scanSync(DIR))
+  await run(path);
