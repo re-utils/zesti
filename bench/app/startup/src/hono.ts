5c6dd2c15b8547df6a15d2333415f25419d39a63
aquapi <aquaplmc@gmail.com>
2025-02-04 21:38:23 +0700

    Remove export

--- bench/app/startup/src/hono.ts
+++ bench/app/startup/src/hono.ts
@@ -7,9 +7,7 @@ for (let i = 0; i < 100; i++)
   app.get('/' + i, (c) => c.body('Hi'));
 
 for (let i = 0; i < 25; i++)
-  app.get(`/${i}/:a/${i}`, (c) => c.body(c.req.param("a")!));
+  app.get(`/${i}/:a`, (c) => c.body(c.req.param("a")!));
 
 // Build Hono
 app.fetch(new Request('http://localhost'));
-
-export const serve = app;
--- bench/app/startup/src/zesti.ts
+++ bench/app/startup/src/zesti.ts
@@ -7,6 +7,8 @@ for (let i = 0; i < 100; i++)
   app.get('/' + i, (c) => c.send('Hi'));
 
 for (let i = 0; i < 25; i++)
-  app.get(`/${i}/*/${i}`, (params, c) => c.send(params[0]));
+  app.get(`/${i}/*`, (params, c) => c.send(params[0]));
 
-export const serve = lazyBuild(() => build(app, buildAdapter));
+const output = lazyBuild(() => build(app, buildAdapter));
+// @ts-ignore
+output.fetch(new Request('http://localhost'));
