43ea41c69a1cbb1d26c244f257189e5625b90320
aquapi <aquaplmc@gmail.com>
2025-02-04 21:33:36 +0700

    Make Hono actually build

--- bench/app/startup/src/hono.ts
+++ bench/app/startup/src/hono.ts
@@ -7,6 +7,9 @@ for (let i = 0; i < 100; i++)
   app.get('/' + i, (c) => c.body('Hi'));
 
 for (let i = 0; i < 25; i++)
-  app.get('/:a/' + i, (c) => c.body(c.req.param("a")!));
+  app.get(`/${i}/:a/${i}`, (c) => c.body(c.req.param("a")!));
+
+// Build Hono
+app.fetch(new Request('http://localhost'));
 
 export const serve = app;
--- bench/app/startup/src/zesti.ts
+++ bench/app/startup/src/zesti.ts
@@ -7,6 +7,6 @@ for (let i = 0; i < 100; i++)
   app.get('/' + i, (c) => c.send('Hi'));
 
 for (let i = 0; i < 25; i++)
-  app.get(`/*/${i}`, (params, c) => c.send(params[0]));
+  app.get(`/${i}/*/${i}`, (params, c) => c.send(params[0]));
 
 export const serve = lazyBuild(() => build(app, buildAdapter));
